ac94a325b6904482f96e9ca5cb9f9c8fa168a7db
Shukhrat <losteand@yandex.ru>
2025-09-05 21:49:18 +0300

    класс для многопоточной обработки

---
+++ ParallelTextProcessor.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ParallelTextProcessor {
+
+    public static void main(String[] args) throws InterruptedException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = Main.generateText("aab", 30_000);
+        }
+
+        long startTs = System.currentTimeMillis(); // start time
+        
+        List<Thread> threads = new ArrayList<>();
+        for (String text : texts) {
+            Thread thread = new Thread(new TextProcessor(text));
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+}
